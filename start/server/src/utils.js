189f81f7d25a9ef5c107b232e019235ba64de947
Don Gyver Gabito <gyver.26@gmail.com>
2021-09-13 20:16:44 +0800

    Added apollo server

--- start/server/src/utils.js
+++ start/server/src/utils.js
@@ -1,4 +1,4 @@
-const SQL = require('sequelize');
+const SQL = require("sequelize");
 
 module.exports.paginateResults = ({
   after: cursor,
@@ -10,7 +10,7 @@ module.exports.paginateResults = ({
   if (pageSize < 1) return [];
 
   if (!cursor) return results.slice(0, pageSize);
-  const cursorIndex = results.findIndex(item => {
+  const cursorIndex = results.findIndex((item) => {
     // if an item has a `cursor` on it, use that, otherwise try to generate one
     let itemCursor = item.cursor ? item.cursor : getCursor(item);
 
@@ -23,25 +23,19 @@ module.exports.paginateResults = ({
       ? []
       : results.slice(
           cursorIndex + 1,
-          Math.min(results.length, cursorIndex + 1 + pageSize),
+          Math.min(results.length, cursorIndex + 1 + pageSize)
         )
     : results.slice(0, pageSize);
 };
 
 module.exports.createStore = () => {
-  const Op = SQL.Op;
-  const operatorsAliases = {
-    $in: Op.in,
-  };
-
-  const db = new SQL('database', 'username', 'password', {
-    dialect: 'sqlite',
-    storage: './store.sqlite',
-    operatorsAliases,
+  const db = new SQL("database", "username", "password", {
+    dialect: "sqlite",
+    storage: "./store.sqlite",
     logging: false,
   });
 
-  const users = db.define('user', {
+  const users = db.define("user", {
     id: {
       type: SQL.INTEGER,
       primaryKey: true,
@@ -53,7 +47,7 @@ module.exports.createStore = () => {
     token: SQL.STRING,
   });
 
-  const trips = db.define('trip', {
+  const trips = db.define("trip", {
     id: {
       type: SQL.INTEGER,
       primaryKey: true,
